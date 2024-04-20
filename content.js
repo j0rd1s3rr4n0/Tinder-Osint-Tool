f4e64d85917beaed23b564c03b98763619998b9f
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2024-04-20 19:24:28 +0200

    Actualizar contenido.js con cambios en la lógica de búsqueda de usuarios

--- content.js
+++ content.js
@@ -14,6 +14,8 @@
 	3. Utiliza la información de manera ética y legal, y respeta la privacidad de las personas.
 */
 
+if (typeof first_user_passed === 'undefined') {var first_user_passed = false;}else {first_user_passed = true;}
+
 // Función para agregar un nuevo usuario al localStorage
 function addUserToLocalStorage(name, age) {
     // Obtener los usuarios existentes del localStorage
@@ -125,7 +127,33 @@ function findUser(){
 	if(find && (user_to_search!="")){
 		//Check if can do unlikes
 		if(document.querySelector("div.recsCardboard__cardsContainer.H\\(100\\%\\).Pos\\(r\\).Z\\(1\\) > div > div.Pos\\(a\\).B\\(0\\).Iso\\(i\\).W\\(100\\%\\).Start\\(0\\).End\\(0\\) > div > div.Mx\\(a\\).Fxs\\(0\\).Sq\\(70px\\).Sq\\(60px\\)--s.Bd.Bdrs\\(50\\%\\).Bdc\\(\\$c-ds-border-gamepad-nope-default\\) > button")){
-			var name = document.querySelector("div.recsCardboard__cardsContainer.H\\(100\\%\\).Pos\\(r\\).Z\\(1\\) > div > div.Tcha\\(n\\).Bdbw\\(--recs-gamepad-height\\).Bdbc\\(t\\).Bdbs\\(s\\).Bgc\\(\\#000\\).Wc\\(\\$transform\\).Prs\\(1000px\\).Bfv\\(h\\).Ov\\(h\\).W\\(100\\%\\).StretchedBox.Bdrs\\(8px\\) > div.Pos\\(a\\).D\\(f\\).Jc\\(sb\\).C\\(\\$c-ds-text-primary-overlay\\).Ta\\(start\\).W\\(100\\%\\).Ai\\(fe\\).B\\(0\\).P\\(8px\\)--xs.P\\(16px\\).P\\(20px\\)--l.Cur\\(p\\).focus-button-style > div > div > div > div > div.Fz\\(\\$l\\).Trsdu\\(\\$fast\\).Maw\\(80\\%\\) > div > div > span").innerText;
+			var pers = document.querySelectorAll(".focus-button-style > div > div > div > div > div > div > div > span");
+            // Crear un objeto con las claves deseadas
+            var customKeys = {};
+
+            if (pers.length === 3) {
+                customKeys = {
+                    actual: pers[1],
+                    next: pers[0],
+                    past: pers[2]
+                };
+            } else if (pers.length === 2) {
+                if(first_user_passed){
+                customKeys = {
+                    actual: pers[0],
+                    past: pers[1]
+                };
+                }else{
+                    customKeys = {
+                    actual: pers[1],
+                    past: pers[0],
+                    next: undefined
+                };
+                }
+            }
+
+            var name = customKeys.actual.innerText;
+            
 			if(document.querySelector("div.recsCardboard__cardsContainer.H\\(100\\%\\).Pos\\(r\\).Z\\(1\\) > div > div.Tcha\\(n\\).Bdbw\\(--recs-gamepad-height\\).Bdbc\\(t\\).Bdbs\\(s\\).Bgc\\(\\#000\\).Wc\\(\\$transform\\).Prs\\(1000px\\).Bfv\\(h\\).Ov\\(h\\).W\\(100\\%\\).StretchedBox.Bdrs\\(8px\\) > div.Pos\\(a\\).D\\(f\\).Jc\\(sb\\).C\\(\\$c-ds-text-primary-overlay\\).Ta\\(start\\).W\\(100\\%\\).Ai\\(fe\\).B\\(0\\).P\\(8px\\)--xs.P\\(16px\\).P\\(20px\\)--l.Cur\\(p\\).focus-button-style > div > div > div > div > div.Fz\\(\\$l\\).Trsdu\\(\\$fast\\).Maw\\(80\\%\\) > div > span:nth-child(3)")){
             var age = document.querySelector("div.recsCardboard__cardsContainer.H\\(100\\%\\).Pos\\(r\\).Z\\(1\\) > div > div.Tcha\\(n\\).Bdbw\\(--recs-gamepad-height\\).Bdbc\\(t\\).Bdbs\\(s\\).Bgc\\(\\#000\\).Wc\\(\\$transform\\).Prs\\(1000px\\).Bfv\\(h\\).Ov\\(h\\).W\\(100\\%\\).StretchedBox.Bdrs\\(8px\\) > div.Pos\\(a\\).D\\(f\\).Jc\\(sb\\).C\\(\\$c-ds-text-primary-overlay\\).Ta\\(start\\).W\\(100\\%\\).Ai\\(fe\\).B\\(0\\).P\\(8px\\)--xs.P\\(16px\\).P\\(20px\\)--l.Cur\\(p\\).focus-button-style > div > div > div > div > div.Fz\\(\\$l\\).Trsdu\\(\\$fast\\).Maw\\(80\\%\\) > div > span:nth-child(3)").innerText*1;
             }else{var age = -1;}
@@ -154,7 +182,7 @@ function findUser(){
 	}
 }
 
-setInterval(findUser,700);
+setInterval(findUser,250);
 
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
